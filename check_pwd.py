629eec47994962562f2c54112d8fb2919a9a99c7
Kevin Joy <joykm88@gmail.com>
2021-02-11 00:56:26 -0500

    made check_pwd return false if str doesn't have at least 1 special character

--- check_pwd.py
+++ check_pwd.py
@@ -2,7 +2,7 @@ import re
 
 def check_pwd(str):
 
-    pattern = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)")
+    pattern = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)" + "(?=.*[-~`!@#$%^&*()_+=]).+$")
 
     if (len(str) < 8 or len(str) > 20) or not re.search(pattern, str):
         return False
