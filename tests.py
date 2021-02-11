408a0bcd412111cb16721ee7bc996fcec56e50b2
Kevin Joy <joykm88@gmail.com>
2021-02-11 00:31:31 -0500

    wrote test1 to see if an empty string is rejected correctly

--- tests.py
+++ tests.py
@@ -2,7 +2,11 @@ import unittest
 from check_pwd import check_pwd
 
 class TestCase(unittest.TestCase):
-    pass
+
+    # test that null string returns false
+    def test1(self):
+        input = ""
+        self.assertFalse(check_pwd(input))
 
 if __name__ == '__main__':
     unittest.main()
