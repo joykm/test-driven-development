30f39509ce9b98ba46d664beeba82afd59229f33
Kevin Joy <joykm88@gmail.com>
2021-02-11 00:47:07 -0500

    made check_pwd return false if str doesn't have at least 1 uppercase letter

--- check_pwd.py
+++ check_pwd.py
@@ -2,7 +2,7 @@ import re
 
 def check_pwd(str):
 
-    pattern = re.compile("^(?=.*[a-z])")
+    pattern = re.compile("^(?=.*[a-z])(?=.*[A-Z])")
 
     if (len(str) < 8 or len(str) > 20) or not re.search(pattern, str):
         return False
